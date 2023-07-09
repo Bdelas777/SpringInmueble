17643912068e789425484ca6fdc2d313c34ba1ac
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-07-08 19:27:43 -0600

    Ejemplo

---
+++ src/main/java/com/bdelas/springbootmicroservice1inmueble2/SpringBootMicroservice1Inmueble2Application.java
@@ -0,0 +1,13 @@
+package com.bdelas.springbootmicroservice1inmueble2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice1Inmueble2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice1Inmueble2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdelas/springbootmicroservice1inmueble2/SpringBootMicroservice1Inmueble2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdelas.springbootmicroservice1inmueble2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservice1Inmueble2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
